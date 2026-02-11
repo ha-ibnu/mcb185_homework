4b204cb1e13a6887af87d695003c94d853fed497
ha-ibnu <halim.ibnu.hd2@bs.naist.jp>
2026-02-12 01:33:21 +0900

    unit1.3

---
+++ 10demo.py
@@ -0,0 +1,23 @@
+# 10demo.py by Ibnu
+
+import math
+
+print("hello, again") #greeting
+print(1.5e-64)
+
+print(pow(2,3))
+
+print(round(2.4355553, ndigits=3))
+
+print(round((2**0.5), ndigits=2))
+
+print(pow(2, 3))
+
+print("print(math.bla bla)")
+print(math.inf)
+
+print(math.pow(2, 7))
+
+print(math.sqrt(8))
+
+print(round(math.log(3.6), ndigits = 2))
