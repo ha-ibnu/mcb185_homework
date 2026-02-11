602ead9375347435531fb9b56548259ea31b1b82
ha-ibnu <130028773+ha-ibnu@users.noreply.github.com>
2026-02-11 16:09:13 +0900

    Update 04spellingbee.sh

--- 04spellingbee.sh
+++ 04spellingbee.sh
@@ -1,3 +1,4 @@
 ln -s ~/Code/MCB185/data/dictionary.gz ./dic.gz
+gunzip -c dic.gz |grep -E "^.{4,}$"| grep -E "r"|grep -E ^[oznraic]+$
 gunzip -c dic.gz |grep -E "^.{4,}$"| grep -E "r"|grep -E ^[oznraic]+$|wc -l
 rm dic.gz
