3c63bb457511ad609dbec1b09296affdf76d0096
ha-ibnu <halim.ibnu.hd2@bs.naist.jp>
2026-02-14 20:16:51 +0900

    new

--- 10demo.py
+++ 10demo.py
@@ -83,7 +83,7 @@ print(tri_a(2,3))
 print()
 print("#Practice")
 print()
-print("1. Conversion F to C, vice-versa")
+print("Conversion F to C, vice-versa")
 def temp_f(c): return (c*9/5)+32
 def temp_c(f): return (f-32)*5/9
 def temp(val, unit):
@@ -111,3 +111,102 @@ print(round(speed(4, "KPH")), round(speed(100, "MPH")), sep="; ")
 def distance(x1, y1, x2, y2):
     return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 print(distance(2,3,-1,2))
+
+print()
+## NUMBERS
+
+print("#strings")
+#example
+s = 'hello world'
+print(s, type(s))
+
+print("#conditonal")
+#example
+p = 2
+q = 2.3
+if p == q:
+    print('p equals q')
+else: print('unequals', p, q, sep= "; ")
+
+def is_even(x):
+    if x % 2 == 0: return True
+    return False
+print(is_even(4))
+print(is_even(5))
+
+#Boolean
+a = 4
+b = 5
+c = 5
+c = b == a
+print(c, type(c))
+
+if a < b:
+    print('a < b')
+elif a > b:
+    print('a > b')
+else:
+    print('a == b')
+    
+#Boolean chains
+
+if a < b or a > b: print('all things being equal, a n b are not')
+if a < b and a > b: print('you are living in a strange world')
+if not False: print(True)
+
+#Floating point warning
+a = 0.3 
+b = 0.1 * 3
+if a < b: print('a < b')
+elif a > b: print('a > b')
+else: print('a == b')
+
+print(abs(a - b))
+if abs(a-b) < 1e-9: print('close enough')
+if math.isclose(a, b): print('close enough')
+
+#String comparisons
+s1 = 'a'
+s2 = 'b'
+s3 = 'A'
+
+if s1 < s2: print('a < b')
+if s2 < s3: print('a <  A')
+
+#mismatched type error
+#a = 1
+#b = '2'
+#if a < b: print('a < b')
+
+#nonetype
+def silly(m, x, b):
+    y = m * x + b
+print(silly(2, 3, 5), type(silly(2, 3, 5)))
+
+#More function practices
+def is_integer(a):
+    if a // 1 == a or a % 1 == 0: return True
+    return False
+    
+print(is_integer(1), is_integer(2.0))
+
+def is_prob(a): return 0 <= a <= 1
+
+print(is_prob(1))
+
+def mol_wight(DNA):
+    if   DNA == "A": return 2
+    elif DNA == "T": return 1
+    elif DNA == "C": return 3
+    elif DNA == "G": return 4
+    else:            return None
+print(mol_wight('A'))
+
+
+def comp_DNA(DNA):
+    if   DNA == "A": return "T"
+    elif DNA == "T": return "A"
+    elif DNA == "C": return "G"
+    elif DNA == "G": return "C"
+    else:            return None
+print(comp_DNA('A'))
